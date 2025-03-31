a3d22070cdd9058897059de96834cc6cbfff2cb7
Libby Eilbert <elizabeth.eilbert@gmail.com>
2025-03-30 22:12:43 -0700

    fixed updateGame

--- routes/games.js
+++ routes/games.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createGame, getGames, getGameById } = require("../controllers/gamesController");
+const { createGame, getGames, getGameById, updateGame } = require("../controllers/gamesController");
 
 router.post("/", createGame);
 router.get("/", getGames);
+router.get("/:id", getGameById);
 router.put("/:id", updateGame);
-
 
 module.exports = router;
