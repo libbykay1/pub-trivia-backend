153b78b93bcbe44fb4e1420eb1a230b0ac9ec71b
Libby Eilbert <elizabeth.eilbert@gmail.com>
2025-04-04 21:51:51 -0700

    added getGameByCode

--- routes/games.js
+++ routes/games.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { createGame, getGames, getGameById, updateGame, deleteGame } = require("../controllers/gamesController");
+const { createGame, getGames, getGameById, updateGame, deleteGame, getGameByCode } = require("../controllers/gamesController");
 
 router.post("/", createGame);
 router.get("/", getGames);
 router.get("/:id", getGameById);
 router.put("/:id", updateGame);
-router.delete("/:id", deleteGame)
+router.delete("/:id", deleteGame);
+router.get("/code/:code", getGameByCode);
+
 
 module.exports = router;
