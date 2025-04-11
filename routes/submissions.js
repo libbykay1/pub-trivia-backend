101cbdfe81173c09d276ad9de9b7784d6580f865
Libby Eilbert <elizabeth.eilbert@gmail.com>
2025-04-11 13:51:09 -0700

    updateSubmission

--- routes/submissions.js
+++ routes/submissions.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const {
-    submitAnswers
+    submitAnswers,
+    updateSubmission
 } = require("../controllers/submissionsController");
 
 router.post("/:code/:roundIndex", submitAnswers);
+router.put('/:code/rounds/:roundIndex/submissions/:teamId/update', updateSubmission);
 
 
 module.exports = router;
